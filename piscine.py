a76904f44491c653cd4aaa441c3fef89bc465b2f
nostyn6 <nostyn6@gmail.com>
2025-11-27 15:14:44 +0100

    Add files via upload

---
+++ piscine.py
@@ -0,0 +1,25 @@
+#
+#   Projet de développement Python 
+#   Gestionnaire d'utilisateurs d'une piscine 
+#
+
+print("--- Gestionnaire d'utilisateurs d'une piscine ---")
+
+liste = []
+commande = ''
+
+while commande != 'exit':
+    commande = input("Que faut-il faire ? ")
+
+    if commande == 'ajout':
+        a = input("Qui nage ? ")
+        b = input("quelle nage ? ")
+        c = input("combien de longueur ? ")
+        liste.append((a,b,c))
+   
+    if commande == 'liste':
+        for elt in liste:
+            print(f"Prénom {elt[0]}, nage {elt[1]}, longueur {elt[2]}")
+
+# Fin du programme
+# test
