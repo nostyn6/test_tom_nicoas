bb717e4662ca912cc0b313ad942d12256eb5be5f
nostyn6 <nostyn6@gmail.com>
2025-11-27 16:08:57 +0100

    ajout elt 3 (date)

--- piscine.py
+++ piscine.py
@@ -13,18 +13,25 @@ while commande != 'exit':
 
     if commande == 'ajout':
         a = input("Qui nage ? ")
-        b = input("quelle nage ? ")
-        c = input("combien de longueur ? ")
-        liste.append((a,b,c))
+        b = input("Quelle nage ? ")
+        c = input("Combien de longueur ? ")
+        d = input("Quel jour ? YY_MM_DD")
+        liste.append((a,b,c,d))
    
     if commande == 'liste':
         for elt in liste:
-            print(f"Prénom {elt[0]}, nage {elt[1]}, longueur {elt[2]}")
+            print(f"Prénom {elt[0]}, nage {elt[1]}, longueur {elt[2]}, le {elt[3]}")
 
     if commande == 'nageur':
         nageur = input("Qui nage ? ")
         for elt in liste:
             if nageur == elt[0]:
-                print(nageur, "nage du", elt[1])
+                print(f"{elt[0]} nage du {elt[1]}, le {elt[3]}")
+
+    if commande == 'nage':
+        nage = input("Quelle nage ? ")
+        for elt in liste:
+            if nage == elt[1]:
+                print(f"{nage}, utiliser par, {elt[0]}, le {elt[3]}")
 
 # Fin du programme
